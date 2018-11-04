096843168319c2b2cca5a3925dc2d4ab96a50ed1
Rafal Leszko <rafal@hazelcast.com>
2018-11-04 20:31:33 +0100

    Fix GeoDumper

--- src/main/java/com/leszko/geodump/GeoDumper.java
+++ src/main/java/com/leszko/geodump/GeoDumper.java
@@ -1,8 +1,5 @@
 package com.leszko.geodump;
 
-import com.leszko.geodump.GeoDump.Post;
-import com.leszko.geodump.GeoDump.TripStats;
-
 import java.io.IOException;
 
 class GeoDumper {
@@ -19,27 +16,9 @@ class GeoDumper {
 
     void dump()
             throws IOException {
-//        System.out.println(geoCrawler.readOverview());
-//        System.out.println(geoCrawler.readTrip("http://marianka.geoblog.pl/podroz/13157/na-kolach-za-kolo-rowerem-na-nordkapp"));
-        System.out.println(geoCrawler.readPost("http://marianka.geoblog.pl/wpis/110784/tuz-pod-kolem-polarnym"));
-
-//        String url = String.format(TEMPLATE_URL, username);
-//        List<String> trips = geoCrawler.fetchTrips(url);
-//        String tripUrl = "http://marianka.geoblog.pl/podroz/13157/na-kolach-za-kolo-rowerem-na-nordkapp";
-        //        List<String> posts = fetchPosts(tripUrl);
-//        String postUrl = "http://marianka.geoblog.pl/wpis/109743/nocleg-wreszcie-na-plazy";
-        //        posts.forEach(System.out::println);
-//        Stats stats = geoCrawler.parseStats(tripUrl);
-//        System.out.println(stats);
-
-//        Post post = geoCrawler.parsePost(postUrl);
-//        System.out.println(post);
-        //        fetchMedia(postUrl).stream()
-        //                           .map(link -> extractMediaLink(link))
-        //                           .forEach(System.out::println);
-
+        //        System.out.println(geoCrawler.readOverview());
+        //        System.out.println(geoCrawler.readTrip("http://marianka.geoblog.pl/podroz/13157/na-kolach-za-kolo-rowerem-na-nordkapp"));
+        //        System.out.println(geoCrawler.readPost("http://marianka.geoblog.pl/wpis/110784/tuz-pod-kolem-polarnym"));
     }
-
-
 
 }
